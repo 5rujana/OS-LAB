adaff2d939f48e159b3dbb6c74a0cb0ab8199d3e
Gayatri <gayatri.22bcb7012@vitapstudent.ac.in>
2024-05-07 06:05:45 +0530

    added bankers algo

--- lab8/bankersAlgo.c
+++ lab8/bankersAlgo.c
@@ -1,59 +1,91 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<pthread.h>
-#include<semaphore.h>
-#include<unistd.h>
+//bankers algorithm
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
 
+int n, m;
+int *available, **max, **allocation, **need;
 sem_t mutex;
-sem_t empty;
-sem_t full;
-int buffer[5];
-int in = 0;
-int out = 0;
 
-void *producer(void *arg)
+void *process(void *arg)
 {
-    int item;
-    for(int i=0; i<5; i++)
+    int p = *((int *)arg);
+    int i;
+    sem_wait(&mutex);
+    printf("Process %d is requesting resources\n", p);
+    for (i = 0; i < m; i++)
     {
-        item = rand();
-        sem_wait(&empty);
-        sem_wait(&mutex);
-        buffer[in] = item;
-        printf("Producer produced item %d at position %d\n", item, in);
-        in = (in+1)%5;
-        sem_post(&mutex);
-        sem_post(&full);
-        sleep(1);
+        if (need[p][i] > available[i])
+        {
+            printf("Process %d is waiting\n", p);
+            sem_post(&mutex);
+            return NULL;
+        }
     }
-}
-
-void *consumer(void *arg)
-{
-    int item;
-    for(int i=0; i<5; i++)
+    for (i = 0; i < m; i++)
     {
-        sem_wait(&full);
-        sem_wait(&mutex);
-        item = buffer[out];
-        printf("Consumer consumed item %d from position %d\n", item, out);
-        out = (out+1)%5;
-        sem_post(&mutex);
-        sem_post(&empty);
-        sleep(1);
+        available[i] -= need[p][i];
+        allocation[p][i] += need[p][i];
+        need[p][i] = 0;
     }
+    printf("Process %d has been allocated resources\n", p);
+    sem_post(&mutex);
+    return NULL;
 }
 
 int main()
 {
-    pthread_t prod, cons;
+    int i, j;
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+    printf("Enter the number of resources: ");
+    scanf("%d", &m);
+    available = (int *)malloc(m * sizeof(int));
+    max = (int **)malloc(n * sizeof(int *));
+    allocation = (int **)malloc(n * sizeof(int *));
+    need = (int **)malloc(n * sizeof(int *));
+    for (i = 0; i < n; i++)
+    {
+        max[i] = (int *)malloc(m * sizeof(int));
+        allocation[i] = (int *)malloc(m * sizeof(int));
+        need[i] = (int *)malloc(m * sizeof(int));
+    }
+    printf("Enter the available resources: ");
+    for (i = 0; i < m; i++)
+    {
+        scanf("%d", &available[i]);
+    }
+    printf("Enter the maximum resources: ");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            scanf("%d", &max[i][j]);
+        }
+    }
+    printf("Enter the allocated resources: ");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            scanf("%d", &allocation[i][j]);
+            need[i][j] = max[i][j] - allocation[i][j];
+        }
+    }
     sem_init(&mutex, 0, 1);
-    sem_init(&empty, 0, 5);
-    sem_init(&full, 0, 0);
-    pthread_create(&prod, NULL, producer, NULL);
-    pthread_create(&cons, NULL, consumer, NULL);
-    pthread_join(prod, NULL);
-    pthread_join(cons, NULL);
+    pthread_t tid[n];
+    int p[n];
+    for (i = 0; i < n; i++)
+    {
+        p[i] = i;
+        pthread_create(&tid[i], NULL, process, &p[i]);
+    }
+    for (i = 0; i < n; i++)
+    {
+        pthread_join(tid[i], NULL);
+    }
+    sem_destroy(&mutex);
     return 0;
 }
-
